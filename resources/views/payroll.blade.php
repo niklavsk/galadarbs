67639354efa20289930df4c1301ff5cacdcd680e
niklavsk <niklavsk36@gmail.com>
2020-06-11 16:58:28 +0300

    Create payroll.blade.php

---
+++ resources/views/payroll.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+@section('content')
+
+    <h4>Maksājums</h4>
+
+    <table>
+        <tr>
+            <th>Personas kods</th>
+            <th>Adrese</th>
+            <th>Tālrunis</th>
+            <th>Epasts</th>
+        </tr>
+        <tr>
+            <td>{{$employee->pk}}</td>
+            <td>{{$employee->adrese}}</td>
+            <td>{{$employee->talrunis}}</td>
+            <td>{{$employee->epasts}}</td>
+        </tr>
+    </table>
+
+    <table>
+        <tr>
+            <th>Amats</th>
+            <th>Likme</th>
+            <th>Stundu skaits</th>
+            <th>Izsniegšanas datums</th>
+        </tr>
+        <tr>
+            <td>{{$payroll->amats}}</td>
+            <td>{{$payroll->likme}}</td>
+            <td>{{$payroll->stundu_sk}}</td>
+            <td>{{$payroll->izsniegsanas_datums}}</td>
+        </tr>
+    </table>
+
+    <a href="{{ url('edit/payroll', $payroll->id) }}" class="btn">Rediģēt maksājumu</a>
+    <a href="{{ url('destroy/payroll', $payroll->id) }}" class="btn">Izdzēst maksājumu</a>
+@endsection
+
